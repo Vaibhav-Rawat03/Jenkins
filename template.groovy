b5741e210cc0a096948fef99eecfa3486a242fae
Vaibhav Rawat <111760496+Vaibhav-Rawat03@users.noreply.github.com>
2025-02-02 00:32:24 +0530

    Update template.groovy

--- template.groovy
+++ template.groovy
@@ -32,7 +32,7 @@ echo "DOinggg....................."
         } else {
             error("Filename format did not match expected pattern.")
         }
-echo "${env[lastProjectHashEnv]}" 
+echo """${env."${lastProjectHashEnv}"}"""  
     }
 }
 
